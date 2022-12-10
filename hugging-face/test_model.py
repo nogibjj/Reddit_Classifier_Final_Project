2edbe9ba11cd0d25a2e97cdb93e0f1fdf49247d6
Michelle Li <70456530+michellejieli@users.noreply.github.com>
2022-12-10 03:59:51 +0000

    add reddit data

--- hugging-face/test_model.py
+++ hugging-face/test_model.py
@@ -3,6 +3,10 @@ from transformers import pipeline
 # classification
 text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
 text2 = "fucking hell pissing me off"
+text3 = "I see you’ve set aside this special time to humiliate yourself in public."
+text4 = "Wow, congratulations! So excited for you!"
 classifier = pipeline("sentiment-analysis", model="michellejieli/NSFW_text_classifier")
 print(classifier(text))
 print(classifier(text2))
+print(classifier(text3))
+print(classifier(text4))
